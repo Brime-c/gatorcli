d1fab302df3b1ebf0ca3410e6ed14bca9043e1e4
Brime-c <olabarriaga.felipe@gmail.com>
2026-04-25 23:03:37 -0300

    Read and Write

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,14 @@
 package config
 
 import (
+	"encoding/json"
 	"os"
 	"path/filepath"
 )
 
 type Config struct {
-	db_url *string `json:"db_url"`
+	DbUrl           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
 }
 
 const configFileName = ".gatorconfig.json"
@@ -21,6 +23,32 @@ func getConfigFilePath() (string, error) {
 	return path, nil
 }
 
+func Read() (Config, error) {
+	path, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return Config{}, err
+	}
+	var cfg Config
+	err = json.Unmarshal(data, &cfg)
+
+	if err != nil {
+		return Config{}, err
+	}
+	return cfg, nil
+}
+
 func write(cfg Config) error {
-	return nil
+	path, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+	jsonData, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(path, jsonData, 0644)
 }
