00aa8aaca13a3a8d95967c5d53903579537f806c
Brime-c <olabarriaga.felipe@gmail.com>
2026-06-01 00:15:34 -0300

    follow fixed

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"github.com/Brime/gatorcli/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerFollow(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		fmt.Errorf("no command provided")
 	}
+
+	feed, err := s.db.GetFeedByURL(context.Background(), cmd.args[0])
+	if err != nil {
+		return err
+	}
+	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return err
+	}
+
+	follow, err := s.db.CreateFeedFollow(context.Background(), database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		UserID:    user.ID,
+		FeedID:    feed.ID,
+	})
+
+	fmt.Println(follow.FeedName)
+	fmt.Println(follow.UserName)
+	return nil
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -19,3 +19,7 @@ feeds.url,
 users.name AS user_name
 FROM feeds
 JOIN users ON feeds.user_id = users.id;
+
+-- name: GetFeedByURL :one
+SELECT * FROM feeds
+WHERE URL = $1;
